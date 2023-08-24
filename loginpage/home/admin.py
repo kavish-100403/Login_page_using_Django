7f6f4340eb203a29263c4524300caf03610c5464
kavish-100403 <89000018+kavish-100403@users.noreply.github.com>
2023-08-24 23:45:49 +0530

    Created Database and connected it to the SignUp Page.

--- loginpage/home/admin.py
+++ loginpage/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
+from home.models import Register
+
 # Register your models here.
--- loginpage/home/views.py
+++ loginpage/home/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from django.contrib.auth import logout
-from django.contrib.auth import login
+from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Register
+from django.contrib import messages
+
+
 
 # Create your views here.
 
 def mainpage(request):
     return render(request,'mainpage.html')
 def register(request):
-    return render(request,'register.html')
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        number=request.POST.get('number')
+
+        register= Register(name=name,email=email,password=password, number=number,date=datetime.today())
+        register.save()
+    # return render(request,'register.html')
 def form(request):
     return render(request,'fomr.html')
