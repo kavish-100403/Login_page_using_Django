b35364ab1c752868f2a87d8a5ecd20956ca5f084
kavish-100403 <89000018+kavish-100403@users.noreply.github.com>
2023-08-26 13:18:51 +0530

    Created a Database to store the basic information and Connected the SignUp page to the Database

--- loginpage/home/models.py
+++ loginpage/home/models.py
@@ -6,6 +6,7 @@ class Register(models.Model):
     name=models.CharField(max_length=122)
     email=models.CharField(max_length=122)
     phone=models.CharField(max_length=12)
+    username=models.CharField(max_length=20,default='')
     password=models.CharField(max_length=122)
     date=models.DateField()
 
--- loginpage/home/views.py
+++ loginpage/home/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render,HttpResponse
 from datetime import datetime
 from home.models import Register
 from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout
+from django.contrib.auth import login
+
 
 
 
@@ -13,11 +17,14 @@ def register(request):
     if request.method=="POST":
         name=request.POST.get('name')
         email=request.POST.get('email')
+        username=request.POST.get('username')
         password=request.POST.get('password')
-        number=request.POST.get('number')
+        phone=request.POST.get('phone')
 
-        register= Register(name=name,email=email,password=password, number=number,date=datetime.today())
+        register=Register(name=name,email=email,username=username,password=password,phone=phone,date=datetime.today())
         register.save()
+    return render(request,'register.html')
+    # messages.success(request, "Profile details updated.")
     # return render(request,'register.html')
 def form(request):
-    return render(request,'fomr.html')
+    return render(request,'form.html')
