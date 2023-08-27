16204dee2ea4c0d183f83433ddd90f7a4ea0bbd5
kavish-100403 <89000018+kavish-100403@users.noreply.github.com>
2023-08-27 11:11:53 +0530

    The sign up page is working properly. The user data is now being sent successfully to the database. The other websites are commented out as of now because of some error.

--- loginpage/home/views.py
+++ loginpage/home/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from datetime import datetime
 from home.models import Register
 from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.contrib.auth import logout
 from django.contrib.auth import login
+from django.contrib.auth.models import User
+
 
 
 
@@ -12,7 +14,32 @@ from django.contrib.auth import login
 # Create your views here.
 
 def mainpage(request):
-    return(render(request,'mainpage.html'))
+    return render(request,'mainpage.html')
+
+# def loginUser(request):
+#     if request.method=="POST":
+#         username=request.POST.get('username')
+#         password=request.POST.get('password')
+#         print(username,password)
+#         # print(Register.objects.filter(username=usern))
+
+#         #To check if the user has entered correct credentials
+#         user = authenticate(username=username, password=password)
+#         if user is not None:
+#              # A backend authenticated the credentials
+#             login(request, user)
+#             return redirect("/form")
+#         else:
+#     # No backend authenticated the credentials
+#             return render(request,"mainpage.html")
+        
+#     return(render(request,'mainpage.html'))
+
+# def logoutUser(request):
+#     logout(request)
+#     return redirect("/login")
+
+
 def register(request):
     if request.method=="POST":
         name=request.POST.get('name')
@@ -21,9 +48,14 @@ def register(request):
         password=request.POST.get('password')
         phone=request.POST.get('phone')
 
+        #user will create a new user for logging in to the database
+        user = User.objects.create_user(username,email,password)
+        user.save()
+
+        #register will save the user credentials to the database
         register=Register(name=name,email=email,username=username,password=password,phone=phone,date=datetime.today())
         register.save()
-    return(render(request,'register.html'))
+    return render(request,'register.html')
 
 
     # messages.success(request, "Profile details updated.")
